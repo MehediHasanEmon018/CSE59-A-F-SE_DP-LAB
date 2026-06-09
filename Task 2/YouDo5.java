9178b98126142bd6645735b76d89a3c3e7ced6fd
Mehedi Hasan Emon <mehedihasanemon23mu@gmail.com>
2026-06-09 10:46:45 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,21 @@
 
+public class Main{
+    public static void main(String[] args){
+        Printer printer = new Printer();
+        
+        printer.printData("Hello Java");
+        printer.printData(100);
+    }
+}
+
+class Printer{
+    void printData(String text){
+        System.out.println("Text: "+ text);
+        
+    }
+    void printData(int number){
+        System.out.println("Number: "+ number);
+        
+    }
+}
+
