7d87524d835955ef9d725a716aec7744d237dc1f
Mehedi Hasan Emon <mehedihasanemon23mu@gmail.com>
2026-06-09 10:39:15 +0600

    Update YouDo1.java

--- Task 2/YouDo1.java
+++ Task 2/YouDo1.java
@@ -1,1 +1,18 @@
 
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class ContestStats {
+    int[] scores ={85,90,95};
+    
+    void printFirstScore(){
+        System.out.println("First Score: "+scores[0]);
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        ContestStats contest = new ContestStats();
+        contest.printFirstScore();
+    }
+}
