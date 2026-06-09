1dce937b7fd686c29795d5874b1187b2277ffe50
Mehedi Hasan Emon <mehedihasanemon23mu@gmail.com>
2026-06-09 10:41:09 +0600

    Update YouDo4.java

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,22 @@
 
+class Sensor{
+    String name;
+    int batterylevel;
+    
+    Sensor(String name, int batterylevel){
+        this.name= name;
+        this.batterylevel = batterylevel;
+    }
+    void charge(){
+        batterylevel = 100;
+        System.out.println("Sensor fully charged");
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Sensor sensor = new Sensor(" Tempertature Sensor", 40);
+        sensor.charge();
+        System.out.println("Battery Level: "+sensor.batterylevel);
+    }
+}
