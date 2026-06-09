c580570a74586a1ce8c767558b3b877004054dc8
Mehedi Hasan Emon <mehedihasanemon23mu@gmail.com>
2026-06-09 10:56:40 +0600

    Update YouDo7.java

--- Task 2/YouDo7.java
+++ Task 2/YouDo7.java
@@ -1,1 +1,18 @@
 
+public class Main{
+    public static void main(String[] args){
+        Button button = new Button();
+        button.onClick();
+    }
+}
+
+interface Clickable {
+    void onClick();
+}
+class Button implements Clickable{
+    @Override
+    public void onClick(){
+        System.out.println("Button was clicked!");
+        
+    }
+}
