ba67c7ac0a0a24a90f9e788d921af4b4bf1b9b3f
Mehedi Hasan Emon <mehedihasanemon23mu@gmail.com>
2026-06-23 11:40:47 +0600

    Implement multi-function copier with printer and scanner
    Added interfaces for Printer, Scanner, and Fax, and implemented them in MultiFunctionCopier and BasicPrinter classes.

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,3 +1,36 @@
+public interface Printer {
+    void printDocument();
+}
+
+public interface Scanner {
+    void scanDocument();
+}
+
+public interface Fax {
+    void faxDocument();
+}
+
+class MultiFunctionCopier implements Printer, Scanner, Fax {
+
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
+
+class BasicPrinter implements Printer {
+
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +44,3 @@ public class Main {
         m1.faxDocument();
     }
 }
-
-
