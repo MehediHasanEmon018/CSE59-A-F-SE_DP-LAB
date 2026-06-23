ef20a886dc026a9a736d8821ef1a8d772ee821b3
Mehedi Hasan Emon <mehedihasanemon23mu@gmail.com>
2026-06-23 11:40:05 +0600

    Update Main3.java

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,15 @@
 
+public class Main {
+    public static void main(String[] args) {
+
+        Printer p1 = new BasicPrinter();
+        p1.printDocument();
+
+        MultiFunctionCopier m1 = new MultiFunctionCopier();
+        m1.printDocument();
+        m1.scanDocument();
+        m1.faxDocument();
+    }
+}
+
+
