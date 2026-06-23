3df7dddca8b5dcb23f4a7f5768b078f4c8352b42
Mehedi Hasan Emon <mehedihasanemon23mu@gmail.com>
2026-06-23 10:28:52 +0600

    Implement Employee and Contractor classes with bonus logic

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,43 @@
 
+
+public class Main{
+    public static void main(String[] args){
+        Employee employee = new Employee("Hasan");
+        employee.calculateBonus();
+        
+        Contractor contractor = new Contractor("Rahim");
+        contractor.work();
+    }
+}
+interface BonusEligible{
+    void calculateBonus();
+}
+
+
+
+class TeamMember{
+    protected String name;
+    public TeamMember(String name){
+        this.name=name;
+    }
+}
+
+class Employee extends TeamMember implements BonusEligible{
+    public Employee(String name){
+        super(name);
+    }
+    @Override
+    public void calculateBonus(){
+        System.out.println("Calculating the standard employee bonus...");
+        
+    }
+}
+
+class Contractor extends TeamMember{
+    public Contractor(String name){
+        super(name);
+    }
+    public void work(){
+        System.out.println(name+" is working as a contractor");
+    }
+}
