a3622528572a3eb329e01e2f64b4a8ea1c89aad0
Mehedi Hasan Emon <mehedihasanemon23mu@gmail.com>
2026-06-30 11:06:39 +0600

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,32 @@
 
+public class Main2 {
+
+    interface MessageSender {
+        void sendMessage(String message);
+    }
+
+    static class EmailSender implements MessageSender {
+        @Override
+        public void sendMessage(String message) {
+            System.out.println("Sending email: " + message);
+        }
+    }
+
+    static class NotificationService {
+        private MessageSender messageSender;
+
+        public NotificationService(MessageSender messageSender) {
+            this.messageSender = messageSender;
+        }
+
+        public void alertUser(String msg) {
+            messageSender.sendMessage(msg);
+        }
+    }
+
+    public static void main(String[] args) {
+        MessageSender sender = new EmailSender();
+        NotificationService notificationService = new NotificationService(sender);
+        notificationService.alertUser("Welcome to the system!");
+    }
+}
